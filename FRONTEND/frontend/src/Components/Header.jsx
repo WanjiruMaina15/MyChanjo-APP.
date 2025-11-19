fb55e1c662258e66dc587c8749a2fd987f6b7e3f
WanjiruMaina15 <wanjjirumaina@gmail.com>
2025-11-19 20:55:12 +0300

    FRONTEND

--- FRONTEND/frontend/src/Components/Header.jsx
+++ FRONTEND/frontend/src/Components/Header.jsx
@@ -3,15 +3,15 @@ import { H, P } from "./ReusableComponents/Typography";
 
 export default function Header() {
   return (
-   <header className="sticky top-0 left-0 w-full bg-cyan-300 text-[#0b2545] py-4 shadow-md z-50">
+   <header className="sticky top-0 left-0 w-full bg-[#8134D3] border-b-4 border-[#22D3EE] py-4 shadow-md z-50">
   <div className="container mx-auto text-center px-4">
     
     
-    <H as="h1" className="text-3xl font-bold tracking-wide">
+    <H as="h1" className="text-3xl font-bold tracking-wide text-white">
       MyChanjo
     </H>
     
-    <P className="text-lg mt-1 font-medium tracking-wide leading-snug opacity-90">
+    <P className="text-lg mt-1 font-medium tracking-wide leading-snug text-white/90">
           Keep track of your child’s immunization easily and on time.
         </P>
       </div>
